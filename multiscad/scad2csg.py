5337365f1851754a694f1a08c7e78f9b770d8729
Andrew Plumb <andrew@plumb.org>
2016-04-18 18:34:57 -0400

    Breaking up operators and instances.

--- multiscad/scad2csg.py
+++ multiscad/scad2csg.py
@@ -22,9 +22,12 @@ class OpenSCAD(object):
         self.scad_file = args.scad
         self.csg_file = args.csg
         self.threads = args.threads
-        self.modules = {}
+        self.operators = {}
+        self.instances = {}
         self.hierarchy = []
-        self.module_variants = {} 
+        self.file_stack = []
+        self.operator_variants = {} 
+        self.instance_variants = {} 
         print "Arguments: ",args
         subprocess.check_output([self.openscad,"-o",self.workdir+"/"+self.csg_file,self.scad_file])
         self.read_csg()
@@ -37,47 +40,79 @@ class OpenSCAD(object):
                 module_parts=False
                 parse_line=line.rstrip('\n').rstrip('\r')
                 parse_line=parse_line.lstrip(' ').lstrip('\t')
-                if re.search('{\Z',parse_line) :
-                    hier_level=hier_level+1
-                if re.search('\A}',parse_line) :
-                    hier_level=hier_level-1
                 module_parts=re.match(r"([^(]+)\(([^)]*)\)\s*([;{])",parse_line)
                 if module_parts :
                     module_name=module_parts.group(1)
                     module_params=module_parts.group(2)
                     module_suffix=module_parts.group(3)
-                    variant_name=self.analyze_module(module_name,module_params)
+                    if module_suffix == ";" :
+                        variant_name=self.analyze_instance(module_name,module_params)
+                    else :
+                        hier_level=hier_level+1
+                        variant_name=self.analyze_operator(module_name,module_params)
+                        self.hierarchy.append(variant_name)
                 print "read_csg: ",hier_level," : ",parse_line
                 if module_parts :
                     print "   module_name = ",module_name
                     print "   module_params = ",module_params
                     print "   module_suffix = ",module_suffix
                     print "   variant_name = ",variant_name
+                if re.search('\A}',parse_line) :
+                    hier_level=hier_level-1
+                    self.hierarchy.pop()
                 lines.append(parse_line)
             ins.close()
         print ""
         print "Summary:"
-        for module_name in self.modules.keys() :
-            print "  module '"+module_name+"'"
-            for module_params in self.modules[module_name].keys() :
-                print "    params ("+module_params+")"
-                print "      variant = '"+self.modules[module_name][module_params]['variant']+"'"
-                print "      count = "+str(self.modules[module_name][module_params]['count'])
-    def analyze_module(self,name,params) :
-        print "  analyze_module : name = ",name," params = ",params
-        if not self.modules.has_key(name) :
-            self.modules[name]={}
-            self.module_variants[name]={}
-        if not self.modules[name].has_key(params) :
-            self.modules[name][params]={}
-            self.modules[name][params]['count']=1
-            module_variant_name=name+str(len(self.module_variants[name].keys()))
-            self.modules[name][params]['variant']=module_variant_name
-            self.module_variants[name][module_variant_name]=params
+        for operator_name in self.operators.keys() :
+            print "  operator '"+operator_name+"'"
+            for operator_params in self.operators[operator_name].keys() :
+                print "    params ("+operator_params+")"
+                print "      variant = '"+self.operators[operator_name][operator_params]['variant']+"'"
+                print "      count = "+str(self.operators[operator_name][operator_params]['count'])
+        for instance_name in self.instances.keys() :
+            print "  instance '"+instance_name+"'"
+            for instance_params in self.instances[instance_name].keys() :
+                print "    params ("+instance_params+")"
+                print "      variant = '"+self.instances[instance_name][instance_params]['variant']+"'"
+                print "      count = "+str(self.instances[instance_name][instance_params]['count'])
+                print "      paths:"
+                for path in self.instances[instance_name][instance_params]['paths'] :
+                    print "        "+str(path)
+    def analyze_operator(self,name,params) :
+        print "  analyze_operator : name = ",name," params = ",params
+        if not self.operators.has_key(name) :
+            self.operators[name]={}
+            self.operator_variants[name]={}
+        if not self.operators[name].has_key(params) :
+            self.operators[name][params]={}
+            self.operators[name][params]['count']=1
+            self.operators[name][params]['paths']=[]
+            operator_variant_name=name+str(len(self.operator_variants[name].keys()))
+            self.operators[name][params]['variant']=operator_variant_name
+            self.operator_variants[name][operator_variant_name]=params
         else :
-            self.modules[name][params]['count'] += 1
-            module_variant_name=self.modules[name][params]['variant']
-        return module_variant_name
+            self.operators[name][params]['count'] += 1
+            operator_variant_name=self.operators[name][params]['variant']
+        return operator_variant_name
+    def analyze_instance(self,name,params) :
+        print "  analyze_instance : name = ",name," params = ",params
+        print "                     path = ",self.hierarchy
+        if not self.instances.has_key(name) :
+            self.instances[name]={}
+            self.instance_variants[name]={}
+        if not self.instances[name].has_key(params) :
+            self.instances[name][params]={}
+            self.instances[name][params]['count']=1
+            self.instances[name][params]['paths']=[]
+            instance_variant_name=name+str(len(self.instance_variants[name].keys()))
+            self.instances[name][params]['variant']=instance_variant_name
+            self.instance_variants[name][instance_variant_name]=params
+        else :
+            self.instances[name][params]['count'] += 1
+            instance_variant_name=self.instances[name][params]['variant']
+        self.instances[name][params]['paths'].append(list(self.hierarchy))
+        return instance_variant_name
 
 if __name__ == '__main__':
     print 'Running multiscad...'
